f7d4dc65ca77c2270e2e7986dd112e568d07f16e
Tuatini Godard <tuatinigodard@gmail.com>
2021-02-15 12:07:57 +0200

    Update launch_local_editor.sh

--- launch_local_editor.sh
+++ launch_local_editor.sh
@@ -13,5 +13,8 @@ docker build -t $IMAGE_NAME $DIR
 docker run -d --name ${CONTAINER_NAME} \
     -e url=${URL} -p 2368:2368 -v ${DIR}/ghost_data:/var/lib/ghost/content \
     -v ${STATIC_OUTPUT}:/var/lib/ghost/static $IMAGE_NAME
-
+    
+echo "Waiting for ghost to finish initialization..."
+# We can end up with a page "Site is starting up, please wait a moment then retry." with the CI if we don't do that
+sleep 10
 echo "Launched, now navigate to $URL/ghost"
