214f7f772a54eaa9b688c927856ca45a18ef9f32
Tuatini Godard <tuatinigodard@gmail.com>
2021-02-19 12:33:59 +0200

    Update launch_local_editor.sh
    Add ghost db auto update

--- launch_local_editor.sh
+++ launch_local_editor.sh
@@ -17,4 +17,10 @@ docker run -d --name ${CONTAINER_NAME} \
 echo "Waiting for ghost to finish initialization..."
 # We can end up with a page "Site is starting up, please wait a moment then retry." with the CI if we don't do that
 sleep 10
+
+echo "Updating the ghost db..."
+docker exec ${CONTAINER_NAME} bash -c 'find /var/lib/ghost/content/ -type d -exec chmod 775 {} \;'
+docker exec ${CONTAINER_NAME} bash -c 'find /var/lib/ghost/content/ -type f -exec chmod 664 {} \;'
+docker exec ${CONTAINER_NAME} bash -c 'sudo -u node -H sh -c "cd /var/lib/ghost && ghost update"'
+
 echo "Launched, now navigate to $URL/ghost"
